1a698459125460a01d8ebf8eca311ccc7ccee008
maxonmars <hydrocrub@gmail.com>
2023-01-25 20:09:46 +0700

    add options Alias and tests

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -16,16 +16,33 @@ const rule = require("../../../lib/rules/path-checker"),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parserOptions: {ecmaVersion: 6, sourceType: 'module'}
+});
 ruleTester.run("path-checker", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      filename: '/Users/userName/WebStormProjects/blog/src/entities/Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+      errors: [],
+    },
   ],
 
   invalid: [
     {
-      code: "example",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename: '/Users/userName/WebStormProjects/blog/src/entities/Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
+      errors: [{ message: 'Within a slice, all imports must be relative!'}],
+      options: [
+        {
+          alias: '@'
+        }
+      ]
+    },
+    {
+      filename: '/Users/userName/WebStormProjects/blog/src/entities/Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice'",
+      errors: [{ message: 'Within a slice, all imports must be relative!'}],
     },
   ],
 });
